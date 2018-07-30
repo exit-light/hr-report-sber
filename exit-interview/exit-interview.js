88972060bba785c0f38e9c5d95a56f44bd4d4277
exit-light <mail.nekrasov@mail.ru>
2018-07-30 17:03:16 +0300

    Добавлены недостающие скрипты

--- exit-interview/exit-interview.js
+++ exit-interview/exit-interview.js
@@ -15,19 +15,16 @@ function classExitInterview() {
                 {
                     'label' : 'Текучесть',
                     'value': 17720,
-                    'perc' : 70 + '%',
 
                 },
                 {
                     'label' : 'Текучесть до 11 гр.',
                     'value': 12020,
-                    'perc' : 50 + '%',
 
                 },
                 {
                     'label' : 'Прошли интервью',
                     'value': 8520,
-                    'perc' : 30 + '%',
 
                 },
             ],
@@ -35,45 +32,39 @@ function classExitInterview() {
                 {
                     'label' : 'Позитивно',
                     'value': 17720,
-                    'perc' : 50 + '%',
-
                 },
                 {
                     'label' : 'Неоднозначно',
                     'value': 12020,
-                    'perc' : 25 + '%',
-
                 },
                 {
                     'label' : 'Негативно',
                     'value': 8520,
-                    'perc' : 25 + '%',
-
                 },
             ],
             'results': [
                 {
                     'label' : 'Готовы в будущем вернуться работать в Сбербанк',
-                    'positive': 70 + '%',
-                    'negative' : 30 + '%',
+                    'positive': 70,
+                    'negative' : 30,
 
                 },
                 {
                     'label' : 'Готовы рекомендовать Сбербанк в качестве работодателя',
-                    'positive': 70 + '%',
-                    'negative' : 30 + '%',
+                    'positive': 70,
+                    'negative' : 30,
 
                 },
                 {
                     'label' : 'Готовы рекомендовать свое подразделение и руководителя',
-                    'positive': 70 + '%',
-                    'negative' : 30 + '%',
+                    'positive': 70,
+                    'negative' : 30,
 
                 },
                 {
                     'label' : 'Была ли у непосредственного руководителя возможность повлиять на решение об уходе',
-                    'positive': 70 + '%',
-                    'negative' : 30 + '%',
+                    'positive': 70,
+                    'negative' : 30,
 
                 },
             ],
@@ -85,7 +76,15 @@ function classExitInterview() {
         if(globalSettings.Settings.initial_data) {
             data = JSON.parse(globalSettings.Settings.initial_data);
         }
-        
+        function calcPerc(total, part) {
+            var exp = +(part / (total / 100));
+            return Math.ceil(exp)
+        }
+
+        function splitNum(num) {
+            var re = /(?=\B(?:\d{3})+(?!\d))/g
+            return (String(num).replace(re, ' '));
+        }
         var str = '';
         str +=
         '<div class="container_main">'+ 
@@ -95,22 +94,22 @@ function classExitInterview() {
         '                <div class="block-title">Текучесть</div>'+
         '                <div class="turnover-chart-legend d-flex">'+
         '                    <div class="turnover-chart-legend__item">'+
-        '                        <div class="turnover-chart-legend__item__num">' + data.turnover[0].value + '</div>'+
+        '                        <div class="turnover-chart-legend__item__num">' + splitNum(data.turnover[0].value) + '</div>'+
         '                        <div class="turnover-chart-legend__item__label"><span class="sqr sqr-bg-blue"></span>' + data.turnover[0].label + '</div>'+
         '                    </div>'+
         '                    <div class="turnover-chart-legend__item">'+
-        '                        <div class="turnover-chart-legend__item__num">' + data.turnover[1].value + '</div>'+
+        '                        <div class="turnover-chart-legend__item__num">' + splitNum(data.turnover[1].value) + '</div>'+
         '                        <div class="turnover-chart-legend__item__label"><span class="sqr sqr-bg-aqua"></span>' + data.turnover[1].label + '</div>'+
         '                    </div>'+
         '                    <div class="turnover-chart-legend__item">'+
-        '                        <div class="turnover-chart-legend__item__num">' + data.turnover[2].value + '</div>'+
+        '                        <div class="turnover-chart-legend__item__num">' + splitNum(data.turnover[2].value) + ' ('+ calcPerc(data.turnover[0].value, data.turnover[2].value) +'%)</div>'+
         '                        <div class="turnover-chart-legend__item__label"><span class="sqr sqr-bg-orange"></span> ' + data.turnover[2].label + '</div>'+
         '                    </div>'+
         '                </div>'+
         '                <div class="turnover-chart">'+
-        '                    <div class="turnover-chart__item sqr-bg-blue"></div>'+
-        '                    <div class="turnover-chart__item sqr-bg-aqua"></div>'+
-        '                    <div class="turnover-chart__item sqr-bg-orange"></div>'+
+        '                    <div class="turnover-chart__item sqr-bg-blue" style="width:100%"></div>'+
+        '                    <div class="turnover-chart__item sqr-bg-aqua" style="width:'+ calcPerc(data.turnover[0].value, data.turnover[1].value) +'%"></div>'+
+        '                    <div class="turnover-chart__item sqr-bg-orange" style="width:'+ calcPerc(data.turnover[0].value, data.turnover[2].value) +'%"></div>'+
         '                </div>'+
         '            </div>'+
         '            <div class="block">'+
@@ -145,12 +144,12 @@ function classExitInterview() {
         '                <div class="results-chart">'+
         '                    <div class="results-chart__item">'+
         '                        <div class="results-chart__item__label">'+
-        '                            Готовы в будущем вернуться работать в Сбербанк'+
+        '                            '+ data.results[0].label + 
         '                        </div>'+
         '                        <div class="results-chart__item__graph">'+
         '                            <div class="results-chart__item__graph__inner">'+
-        '                                <div style="width:30%" class="results-chart__item__graph__left"><span class="num">30%</span></div>'+
-        '                                <div style="width:70%" class="results-chart__item__graph__right"><span class="num">70%</span></div>'+
+        '                                <div style="width:'+ data.results[0].positive +'%" class="results-chart__item__graph__left"><span class="num">'+ data.results[0].positive +'%</span></div>'+
+        '                                <div style="width:'+ data.results[0].negative +'%" class="results-chart__item__graph__right"><span class="num">'+ data.results[0].negative +'%</span></div>'+
         '                            </div>'+
         '                        </div>'+
         '                    </div>'+
@@ -159,12 +158,12 @@ function classExitInterview() {
         '                <div class="results-chart">'+
         '                    <div class="results-chart__item">'+
         '                        <div class="results-chart__item__label">'+
-        '                            Готовы рекомендовать Сбербанк в качестве работодателя'+
+        '                            '+ data.results[1].label +
         '                        </div>'+
         '                        <div class="results-chart__item__graph">'+
         '                            <div class="results-chart__item__graph__inner">'+
-        '                                <div style="width:30%" class="results-chart__item__graph__left"><span class="num">30%</span></div>'+
-        '                                <div style="width:70%" class="results-chart__item__graph__right"><span class="num">70%</span></div>'+
+        '                                <div style="width:'+ data.results[1].positive +'%" class="results-chart__item__graph__left"><span class="num">'+ data.results[1].positive +'%</span></div>'+
+        '                                <div style="width:'+ data.results[1].negative +'%" class="results-chart__item__graph__right"><span class="num">'+ data.results[1].negative +'%</span></div>'+
         '                            </div>'+
         '                        </div>'+
         '                    </div>'+
@@ -172,12 +171,12 @@ function classExitInterview() {
         '                <div class="results-chart">'+
         '                    <div class="results-chart__item">'+
         '                        <div class="results-chart__item__label">'+
-        '                            Готовы рекомендовать свое подразделение и руководителя'+
+        '                            '+ data.results[2].label +
         '                        </div>'+
         '                        <div class="results-chart__item__graph">'+
         '                            <div class="results-chart__item__graph__inner">'+
-        '                                <div style="width:30%" class="results-chart__item__graph__left"><span class="num">30%</span></div>'+
-        '                                <div style="width:70%" class="results-chart__item__graph__right"><span class="num">70%</span></div>'+
+        '                                <div style="width:'+ data.results[2].positive +'%" class="results-chart__item__graph__left"><span class="num">'+ data.results[2].positive +'%</span></div>'+
+        '                                <div style="width:'+ data.results[2].negative +'%" class="results-chart__item__graph__right"><span class="num">'+ data.results[2].negative +'%</span></div>'+
         '                            </div>'+
         '                        </div>'+
         '                    </div>'+
@@ -185,12 +184,12 @@ function classExitInterview() {
         '                <div class="results-chart">'+
         '                    <div class="results-chart__item">'+
         '                        <div class="results-chart__item__label">'+
-        '                            Была ли у непосредственного руководителя возможность повлиять на решение об уходе'+
+        '                            '+ data.results[3].label +
         '                        </div>'+
         '                        <div class="results-chart__item__graph">'+
         '                            <div class="results-chart__item__graph__inner">'+
-        '                                <div style="width:30%" class="results-chart__item__graph__left"><span class="num">30%</span></div>'+
-        '                                <div style="width:70%" class="results-chart__item__graph__right"><span class="num">70%</span></div>'+
+        '                                <div style="width:'+ data.results[3].positive +'%" class="results-chart__item__graph__left"><span class="num">'+ data.results[3].positive +'%</span></div>'+
+        '                                <div style="width:'+ data.results[3].negative +'%" class="results-chart__item__graph__right"><span class="num">'+ data.results[3].negative +'%</span></div>'+
         '                            </div>'+
         '                        </div>'+
         '                    </div>'+
